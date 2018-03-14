dd872cc09f7dcbbf9514e9aa5acf2b6ab09323b2
Miltiadis Koustas <31577034+Miltoskoustas@users.noreply.github.com>
2018-03-14 23:57:35 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -44,6 +44,7 @@ var comma = d3.format(",.0f");
 
 function transition(name) {
 	if (name === "all-donations") {
+		click.currentTime=0;
 		click.play();
 		$("#initial-content").fadeIn(250);
 		$("#value-scale").fadeIn(1000);
@@ -54,6 +55,8 @@ function transition(name) {
 		//location.reload();
 	}
 	if (name === "group-by-party") {
+		click.currentTime=0;
+		click.play();
 		$("#initial-content").fadeOut(250);
 		$("#value-scale").fadeOut(250);
 		$("#view-donor-type").fadeOut(250);
@@ -62,6 +65,8 @@ function transition(name) {
 		return partyGroup();
 	}
 	if (name === "group-by-donor-type") {
+		click.currentTime=0;
+		click.play();
 		$("#initial-content").fadeOut(250);
 		$("#value-scale").fadeOut(250);
 		$("#view-party-type").fadeOut(250);
@@ -70,6 +75,8 @@ function transition(name) {
 		return donorType();
 	}
 	if (name === "group-by-money-source")
+		click.currentTime=0;
+		click.play();
 		$("#initial-content").fadeOut(250);
 		$("#value-scale").fadeOut(250);
 		$("#view-donor-type").fadeOut(250);
